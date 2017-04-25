dd1ed8c1345b6229bcc28c2190ea69ec54acb4a3
Radjak <benji2048@hotmail.fr>
2017-04-25 18:59:02 +0200

    Prod

--- controllers/home.controller.js
+++ controllers/home.controller.js
@@ -1,15 +1,25 @@
-app.controller('HomeController', ['$scope', 'leafletData',
-    function($scope, leafletData){
+app.controller('HomeController', ['$scope', 'leafletData', '$http',
+    function($scope, leafletData, $http){
         $scope.message = "HOME";
 		
-		$scope.center = {
-			lat: 51.505,
-			lng: -0.09,
-			zoom: 8
-		};
-		
 		leafletData.getMap('mymap').then(function(map) {
-            L.GeoIP.centerMapOnPosition(map, 15);
+            L.GeoIP.centerMapOnPosition(map, 11);
         });
+		
+		$scope.markers = {};
+		
+		$http({
+			method: 'GET',
+			url: 'https://www.api.benpedia.com/handistress/markers/get.php'
+		}).then(function successCallback(response) {
+			console.log(response);
+			$scope.markers = response.data;
+		}, function errorCallback(response) {
+			console.log(response);
+		});
+		
+		$scope.$on('leafletDirectiveMarker.click', function(e, args){
+			// call api with args.model.id
+		});
     }
 ]);
