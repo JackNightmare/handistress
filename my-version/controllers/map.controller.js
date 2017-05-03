fb5c9b75d04cf688de0060f0c1b4a9fb00a906c0
unknown <lhalter@PCASYIDF106.AUSYNET.FR>
2017-05-03 16:01:03 +0200

    remaniment du bestiaux

--- my-version/controllers/map.controller.js
+++ my-version/controllers/map.controller.js
@@ -1,26 +1,64 @@
 app.controller('mapController', function($scope , Marker, filterFilter){
 	var allMarkers = [];
 
+	/****************************************
+	*** Mise en place de la carte leaflet ***
+	****************************************/
+	angular.extend($scope, {
+		center : {
+			lat: 48.847319,
+			lng: 2.386581,
+			zoom: 12
+		},
+		markers: {},
+		defaults : {
+			scrollWheelZomm: false,
+			zoomControlPosition: 'topright',
+		},
+		events: {
+			map: {
+                enable: ['click', 'drag'], // Les evenements que nous souhaitons ecouté
+                logic: 'emit'
+            }
+        }
+	});
+
+	$scope.$on('leafletDirectiveMap.drag', function(){
+		console.log($scope.center.lat); // On get la latitude
+		console.log($scope.center.lng); // On get la longitude
+	});
+
+	$scope.addMarkers = function(){
+		angular.extend($scope, {
+			markers: {
+				m1: {
+					lat: 51.505,
+					lng: -0.09,
+					message: "I'm a static marker",
+				},
+				m2: {
+					lat: 51,
+					lng: 0,
+					focus: true,
+					message: "Hey, drag me if you want",
+				}
+			}
+		})
+	}
+
+	$scope.removeMarkers = function(){
+		$scope.markers = {};
+	}
+
 	// On recupere les marqueurs
 	$scope.getMarkers = Marker.getMarkers()
 		.then(function(markers){ // Ici tout ce que nous devons faire en cas de succès
 			$scope.getMarkers = markers;
-
-			for(keyMarkers in $scope.getMarkers){
-				info = {
-					lat: parseFloat($scope.getMarkers[keyMarkers].latitude),
-					lng: parseFloat($scope.getMarkers[keyMarkers].longitude),
-					message: $scope.getMarkers[keyMarkers].nameMarker
-				};
-
-				allMarkers.push(info);
-			}
-
+			$scope.addMarkers();
 		}, function(msg){ // Ici action en cas d'erreur
 			console.log(msg);
 		});
 
-	console.log(allMarkers);
 
 	/**********************************
 	*** Fonction filtre de la carte ***
@@ -67,57 +105,4 @@ app.controller('mapController', function($scope , Marker, filterFilter){
 			$scope.filter = 3;
 		}
 	}
-
-
-	/****************************************
-	*** Mise en place de la carte leaflet ***
-	****************************************/
-
-	angular.extend($scope, {
-		center : {
-			lat: 48.847319,
-			lng: 2.386581,
-			zoom: 20
-		},
-		markers: {},
-		defaults : {
-			scrollWheelZomm: false,
-			zoomControlPosition: 'topright',
-		},
-		events: {
-			map: {
-                enable: ['click', 'drag'], // Les evenements que nous souhaitons ecouté
-                logic: 'emit'
-            }
-        }
-	});
-
-	$scope.$on('leafletDirectiveMap.drag', function(){
-		console.log($scope.center.lat); // On get la latitude
-		console.log($scope.center.lng); // On get la longitude
-	});
-
-	$scope.addMarkers = function(){
-		angular.extend($scope, {
-			markers: {
-				m1: {
-					lat: 51.505,
-					lng: -0.09,
-					message: "I'm a static marker",
-				},
-				m2: {
-					lat: 51,
-					lng: 0,
-					focus: true,
-					message: "Hey, drag me if you want",
-				}
-			}
-		})
-	}
-
-	$scope.removeMarkers = function(){
-		$scope.markers = {};
-	}
-
-	$scope.addMarkers();
 });
