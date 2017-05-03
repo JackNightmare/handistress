33ea43b73ba03373a0746161ce4cf34ba6f69a80
unknown <lhalter@PCASYIDF106.AUSYNET.FR>
2017-05-03 10:04:16 +0200

    mise a jour

---
+++ my-version/app.js
@@ -0,0 +1,21 @@
+var app = angular.module('HandiStress', ['ngRoute']);
+
+app.config(function($routeProvider, $locationProvider){
+	$routeProvider
+		.when('/', {
+			templateUrl : 'views/landing.html'
+		})
+		.when('/map', {
+			templateUrl : 'views/map.html',
+			controller : 'mapController'
+		})
+		.when('/add-marker', {
+			templateUrl : 'views/add-marker.html'
+		})
+		.when('/sign-up', {
+			templateUrl : 'views/sign-up.html'
+		})
+		.otherwise({redirectTo: '/'});
+
+		$locationProvider.html5Mode(true);
+});
