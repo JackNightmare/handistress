87eb315a6f9eea9d494aa7ffd057c1675d1100cd
JackNightmare <louis.halter@voila.fr>
2017-05-11 00:14:55 +0200

    mapbox direction first step

--- my-version/controllers/map.controller.js
+++ my-version/controllers/map.controller.js
@@ -89,84 +89,65 @@ app.controller('mapController', function($scope , Marker, filterFilter, leafletD
 			/**************************
 			*** Version MapquestApi ***
 			**************************/
-			leafletData.getMap().then(function(map){
-				trace = MQ.routing.directions()
-					.on('success', function(data) {
-						console.log(data.info.messages);
-					})
-					.on('error', function(data){
-						console.log(data);
-					});
-
-				// permet de supprimer un itinéraire si existe déja
-				if(traceRoute){
-					map.removeLayer(traceRoute);
-				}
-
-				// Permet d'informer les points de départs trajet
-				trace.route({
-					locations: [
-						{ latLng: { lat: parseFloat(startPoint[0]['latitude']), lng: parseFloat(startPoint[0]['longitude']) } },
-						{ latLng: { lat: parseFloat(endPoint[0]['latitude']), lng: parseFloat(endPoint[0]['longitude']) } }
-					],
-					options: {
-						routeType: 'pedestrian',
-						locale: 'fr_FR'
-					}
-				});
-
-				// On ajoute les valeurs
-				traceRoute = MQ.routing.routeLayer({
-					directions: trace,
-					fitBounds: true,
-					zoom: 2,
-				});
-
-				// On ajoute à la carte les informations de l'itinéraire
-				map.addLayer(traceRoute);
-			});
+			// leafletData.getMap().then(function(map){
+			// 	trace = MQ.routing.directions()
+			// 		.on('success', function(data) {
+			// 			console.log(data.info.messages);
+			// 		})
+			// 		.on('error', function(data){
+			// 			console.log(data);
+			// 		});
+			//
+			// 		console.log(startPoint[0]['latitude']+"-"+startPoint[0]['longitude']);
+			// 		console.log(endPoint[0]['latitude']+"-"+endPoint[0]['longitude']);
+			// 	// permet de supprimer un itinéraire si existe déja
+			// 	// if(traceRoute){
+			// 	// 	map.removeLayer(traceRoute);
+			// 	// }
+			//
+			// 	// Permet d'informer les points de départs trajet
+			// 	trace.route({
+			// 		locations: [
+			// 			{ latLng: { lat: parseFloat(startPoint[0]['latitude']), lng: parseFloat(startPoint[0]['longitude']) } },
+			// 			{ latLng: { lat: parseFloat(endPoint[0]['latitude']), lng: parseFloat(endPoint[0]['longitude']) } }
+			// 		],
+			// 		options: {
+			// 			routeType: 'pedestrian',
+			// 			locale: 'fr_FR'
+			// 		}
+			// 	});
+			//
+			// 	// On ajoute les valeurs
+			// 	traceRoute = MQ.routing.routeLayer({
+			// 		directions: trace,
+			// 		fitBounds: true,
+			// 		zoom: 2,
+			// 	});
+			//
+			// 	// On ajoute à la carte les informations de l'itinéraire
+			// 	map.addLayer(traceRoute);
+			// });
 
 			/************************
 			*** Version with OSRM ***
 			************************/
-			// leafletData.getMap().then(function(map){
-			//
-			// 	map.fitBounds([
-			// 		[parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])],
-			// 		[parseFloat(endPoint[0]['latitude']), parseFloat(endPoint[0]['longitude'])]
-			// 	]);
-			//
-			// 	L.Routing.control({
-			// 		waypoints: [
-			// 			L.latLng(parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])),
-			// 			L.latLng(parseFloat(endPoint[0]['latitude']), parseFloat(endPoint[0]['longitude']))
-			// 		],
-			// 		overview: 'pedestrian',
-			// 	}).addTo(map);
-			//
-			// 	L.Routing.Itinerary({
-			// 		pointMarkerStyle : {radius: 5,color: '#03f',fillColor: 'white',opacity: 1,fillOpacity: 0.7},
-			// 		collapsible : true,
-			// 		zoom : 10
-			// 	});
-			// });
+			leafletData.getMap().then(function(map){
+				// map.fitBounds([
+				// 	[parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])],
+				// 	[parseFloat(endPoint[0]['latitude']), parseFloat(endPoint[0]['longitude'])]
+				// ]);
 
-			// valueStart = {
-			// 				lat : parseFloat(startPoint[0]['latitude']),
-			// 				lng : parseFloat(startPoint[0]['longitude']),
-			// 				message : startPoint[0]['nameMarker']
-			// 			};
-			//
-			// valueEnd = {
-			// 				lat : parseFloat(endPoint[0]['latitude']),
-			// 				lng : parseFloat(endPoint[0]['longitude']),
-			// 				message : endPoint[0]['nameMarker']
-			// 			};
-			//
-			// infoTrace.push(valueStart);
-			// infoTrace.push(valueEnd);
-			//
-			// $scope.markers = infoTrace;
+				L.Routing.control({
+					waypoints: [
+						L.latLng(parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])),
+						L.latLng(parseFloat(endPoint[0]['latitude']), parseFloat(endPoint[0]['longitude']))
+					],
+					show: true,
+					language : 'fr',
+				})
+				.addTo(map);
+
+			});
 		}
 	}
 
@@ -203,11 +184,6 @@ app.controller('mapController', function($scope , Marker, filterFilter, leafletD
 	}
 
 	$scope.seeAll = function(){
-
-		// permet de supprimer un itinéraire si existe déja
-		if(traceRoute){
-			map.removeLayer(traceRoute);
-		}
 
 		if($scope.filter == 1){
 			$scope.filter = 0;
