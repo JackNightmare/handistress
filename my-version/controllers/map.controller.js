07f013dd15e3ca065f003927b09cb28a74c78353
JackNightmare <louis.halter@voila.fr>
2017-05-18 23:36:16 +0200

    coup de propre code

--- my-version/controllers/map.controller.js
+++ my-version/controllers/map.controller.js
@@ -86,63 +86,16 @@ app.controller('mapController', function($scope , Marker, filterFilter, leafletD
 
 		if(startPoint.length > 0 && endPoint.length > 0){
 
-			/**************************
-			*** Version MapquestApi ***
-			**************************/
-			// leafletData.getMap().then(function(map){
-			// 	trace = MQ.routing.directions()
-			// 		.on('success', function(data) {
-			// 			console.log(data.info.messages);
-			// 		})
-			// 		.on('error', function(data){
-			// 			console.log(data);
-			// 		});
-			//
-			// 		console.log(startPoint[0]['latitude']+"-"+startPoint[0]['longitude']);
-			// 		console.log(endPoint[0]['latitude']+"-"+endPoint[0]['longitude']);
-			// 	// permet de supprimer un itinéraire si existe déja
-			// 	// if(traceRoute){
-			// 	// 	map.removeLayer(traceRoute);
-			// 	// }
-			//
-			// 	// Permet d'informer les points de départs trajet
-			// 	trace.route({
-			// 		locations: [
-			// 			{ latLng: { lat: parseFloat(startPoint[0]['latitude']), lng: parseFloat(startPoint[0]['longitude']) } },
-			// 			{ latLng: { lat: parseFloat(endPoint[0]['latitude']), lng: parseFloat(endPoint[0]['longitude']) } }
-			// 		],
-			// 		options: {
-			// 			routeType: 'pedestrian',
-			// 			locale: 'fr_FR'
-			// 		}
-			// 	});
-			//
-			// 	// On ajoute les valeurs
-			// 	traceRoute = MQ.routing.routeLayer({
-			// 		directions: trace,
-			// 		fitBounds: true,
-			// 		zoom: 2,
-			// 	});
-			//
-			// 	// On ajoute à la carte les informations de l'itinéraire
-			// 	map.addLayer(traceRoute);
-			// });
-
 			/************************
 			*** Version with OSRM ***
 			************************/
 			leafletData.getMap().then(function(map){
-				// map.fitBounds([
-				// 	[parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])],
-				// 	[parseFloat(endPoint[0]['latitude']), parseFloat(endPoint[0]['longitude'])]
-				// ]);
-				testOption = {
+
+				optionRouting = {
 					profile: 'mapbox/walking'
 				};
 
-				mapboxRouter = L.Routing.mapbox('pk.eyJ1IjoiamFjazE5IiwiYSI6ImNqMms1MGpueTAwMDMyd2x1bHoyMWducXEifQ.2jAcRq_NIGBIaNM3oHNhWg', testOption);
-				console.log(mapboxRouter);
-
+				mapboxRouter = L.Routing.mapbox('pk.eyJ1IjoiamFjazE5IiwiYSI6ImNqMms1MGpueTAwMDMyd2x1bHoyMWducXEifQ.2jAcRq_NIGBIaNM3oHNhWg', optionRouting);
 				L.Routing.control({
 					waypoints: [
 						L.latLng(parseFloat(startPoint[0]['latitude']), parseFloat(startPoint[0]['longitude'])),
