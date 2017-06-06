ed54e3cd353ba85330ac117d5a4ce4000d5b72c6
unknown <lhalter@PCASYIDF106.AUSYNET.FR>
2017-06-06 15:59:02 +0200

    mecanique connexion

--- my-version/controllers/header.controller.js
+++ my-version/controllers/header.controller.js
@@ -1,3 +1,23 @@
 app.controller('headerController', function($scope){
-  
+	
+	/****************************************
+	*** Variables globales pour le Header ***
+	****************************************/
+	$scope.activeFilter = false;
+
+
+	/*****************************************************
+	*** Ici il faut mettre le bousin pour se connecter ***
+	*****************************************************/
+
+	/*************************************
+	*** Fonction pour fermer le filtre ***
+	*************************************/
+	$scope.closeFilter = function(){
+		$scope.activeFilter = false;
+	}
+
+	$scope.openFilter = function(){
+		$scope.activeFilter = true;
+	}
 });
